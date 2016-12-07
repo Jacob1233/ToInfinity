cb2adb64c4997338f9a8f5e86af7ce61d4e6b15f
BuildTools <unconfigured@null.spigotmc.org>
2016-12-06 22:02:59 -0600

    redesign

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This is an 8chan random board finder.
 https://jacob1233.github.io/ToInfinity/
+
+Tread carefully
---
+++ assets/js/infinity.js
@@ -0,0 +1,44 @@
+var sec;
+var sec_ver = '1';
+
+var url_home = 'https://8ch.net/';
+var url_random = 'https://8ch.net/random.php';
+
+var element_iframe = $('iframe');
+var element_privacy = $('#privacy');
+var element_input = $('#input');
+var element_jump_icon = $('#jump');
+
+function init() {
+    sec = checkSecCookie();
+    if (sec == true) {
+        element_privacy.removeClass('text-danger');
+        element_privacy.addClass('text-success');
+        element_iframe.attr('src', url_home);
+    }
+}
+
+function checkSecCookie() {
+    if (Cookies.get('sec_ver') == sec_ver) {
+        return true;
+    }
+    return false;
+}
+
+function setSecCookie() {
+    Cookies.set('sec_ver', sec_ver);
+}
+
+function getRandomBoard() {
+    element_iframe.attr('src', url_random);
+}
+
+function getHomePage() {
+    element_iframe.attr('src', url_home);
+}
+
+function jumpToBoard() {
+    element_iframe.attr('src', url_home + element_input.val());
+    element_input.val('');
+    element_jump_icon.click();
+}
